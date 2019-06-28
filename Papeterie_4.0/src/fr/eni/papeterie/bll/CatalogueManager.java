35f1b9c122d702f4928d1c4bcc29e9fedf177409
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2019-06-28 16:41:30 +0200

    correction

--- Papeterie_4.0/src/fr/eni/papeterie/bll/CatalogueManager.java
+++ Papeterie_4.0/src/fr/eni/papeterie/bll/CatalogueManager.java
@@ -3,6 +3,8 @@ package fr.eni.papeterie.bll;
 import java.util.List;
 
 import fr.eni.papeterie.bo.Article;
+import fr.eni.papeterie.bo.Ramette;
+import fr.eni.papeterie.bo.Stylo;
 import fr.eni.papeterie.dal.ArticleDAO;
 import fr.eni.papeterie.dal.DALException;
 import fr.eni.papeterie.dal.DAOFactory;
@@ -24,11 +26,16 @@ public class CatalogueManager {
 	}
 	/**
 	 * @return the catalogue qui est une liste d'articles
+	 * @throws BLLException 
 	 * @throws DALException 
 	 */
-	public List<Article> getCatalogue() throws DALException {
+	public List<Article> getCatalogue() throws BLLException {
 		
-		return this.daoArticle.selectAll();
+		try {
+			return this.daoArticle.selectAll();
+		} catch (DALException e) {
+			throw new BLLException(" Erreur récupération catalogue ", e);
+		}
 	}
 
 	public void addArticle(Article article) throws BLLException, DALException
@@ -37,14 +44,48 @@ public class CatalogueManager {
 	}
 	public void updateArticle(Article a) throws BLLException {
 		
+		
 	}
 	public void removeArticle(int index) throws BLLException {
 		
 	}
 	public void validerArticle(Article a) throws BLLException {
 		
+		boolean valid = true;
+		StringBuilder sb = new StringBuilder();
+		
+		if(a == null) {
+			valid = false;
+			throw new BLLException("\n\tArticle null.\n");
+		}
+		if(a.getReference() == null || a.getReference().trim().isEmpty()) {
+			sb.append(" La référence article est obligatoire");
+			valid = false;
+		}
+		if(a.getMarque() == null || a.getMarque().trim().isEmpty()) {
+			valid = false;
+		}
+		if(a.getDesignation() == null || a.getDesignation().trim().isEmpty()) {
+			sb.append(" La désignation est obligatoire");
+			valid = false;
+		}
+		if(a instanceof Ramette && ((Ramette) a).getGrammage() <= 0)
+		{
+			sb.append("Le grammage doir avoir une valeur strictement positive.\n");
+			valid = false;
+		}
+		if(a instanceof Stylo && ((Stylo) a).getCouleur().trim().isEmpty())
+		{
+			sb.append("La couleur du stylo doit être renseignée.\n");
+			valid = false;
+		}
+		if(!valid)
+		{
+			throw new BLLException(sb.toString());
+		}
 	}
 	public Article getArticle(int index) throws BLLException {
+		
 		Article article = null;
 		return article;
 		
